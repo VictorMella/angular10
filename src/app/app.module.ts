3e7a85a99fc29124d3bba79274a304ab16afbd54
Victor <v.mellaq@gmail.com>
2020-09-09 17:37:25 -0300

    Clase tareas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule} from '@angular/forms';
 import { DestacarDirective } from './directivas/destacar.directive';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavBarComponent } from './navBar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { TablaComponent } from './dashboard/tabla/tabla.component';
+import { ListaComponent } from './dashboard/lista/lista.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DestacarDirective
+    DestacarDirective,
+    DashboardComponent,
+    NavBarComponent,
+    FooterComponent,
+    TablaComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule,
